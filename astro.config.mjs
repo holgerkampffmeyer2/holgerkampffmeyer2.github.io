29a290d9f4716783b86d33708a495bb8ee9ee84b
Holger Kampffmeyer <holger.kampffmeyer@googlemail.com>
2026-05-23 09:13:10 +0200

    fix: update redirects for rental packages in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,6 +14,14 @@ export default defineConfig({
   trailingSlash: 'never',
   integrations: [sitemap()],
   redirects: {
-  '/dj/': '/djhulk-electronic-music'
+  '/dj/': '/djhulk-electronic-music',
+  '/vermietung/djpaket-fildern/': 'https://soundundlicht-stuttgart.de/vermietung/djpaket-fildern/',
+  '/vermietung/jbl-partybox-300-320/': 'https://soundundlicht-stuttgart.de/vermietung/jbl-partybox-300-320/',
+  '/vermietung/kls-laser-bar/': 'https://soundundlicht-stuttgart.de/vermietung/kls-laser-bar/',
+  '/vermietung/ld-maui-28g3/': 'https://soundundlicht-stuttgart.de/vermietung/ld-maui-28g3/',
+  '/vermietung/led-bossfx-nebelmaschine/': 'https://soundundlicht-stuttgart.de/vermietung/led-bossfx-nebelmaschine/',
+  '/vermietung/partylicht-moving-head/': 'https://soundundlicht-stuttgart.de/vermietung/partylicht-moving-head/',
+  '/vermietung/partypaket-stuttgart/': 'https://soundundlicht-stuttgart.de/vermietung/partypaket-stuttgart/',
+  '/vermietung/veranstaltungspaket-stuttgart/': 'https://soundundlicht-stuttgart.de/vermietung/veranstaltungspaket-stuttgart/'
   }
 });
